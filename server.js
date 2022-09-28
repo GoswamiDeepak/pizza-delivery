6a38a5a22c6eb0037c54c589bc0ee2a4f53eb437
prodigl <35952708+prodigl@users.noreply.github.com>
2022-09-28 13:34:20 +0530

    cart & login & register

--- server.js
+++ server.js
@@ -9,14 +9,29 @@ const PORT = process.env.PORT || 8080;
 //Assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>{
-    res.render("home");
-})
-
 //Set Template Engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render("home");
+})
+
+app.get('/cart',(req,res)=>{
+    res.render("customers/cart");
+})
+
+app.get('/login',(req,res)=>{
+    res.render("auth/login");
+});
+
+app.get('/register',(req,res)=>{
+    res.render("auth/register");
+});
+
+
+console.log(__dirname)
 
 app.listen(PORT , ()=> {
     console.log(`Driving on port ${PORT}`);
